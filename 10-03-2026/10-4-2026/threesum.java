87969731ea0a89d1b7e39cd8bafe46a4a0003596
vinitsingh976 <9.51vinit@gmail.com>
2026-04-13 08:17:19 +0530

    first commit

--- 10-03-2026/10-4-2026/threesum.java
+++ 10-03-2026/10-4-2026/threesum.java
@@ -8,7 +8,7 @@ class Solution {
 
         for (int i = 0; i < nums.length - 2; i++) {
 
-            // Skip duplicates
+            
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
@@ -24,7 +24,7 @@ class Solution {
                     left++;
                     right--;
 
-                    // Skip duplicates
+                   
                     while (left < right && nums[left] == nums[left - 1]) left++;
                     while (left < right && nums[right] == nums[right + 1]) right--;
 
